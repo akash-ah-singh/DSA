3d6f7c91bcb1c7c2275ce5232c539efb0d7ab048
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-08-05 11:50:01 +0530

    pass By Value

---
+++ passByValue.java
@@ -0,0 +1,19 @@
+package Conditional_Loops;
+
+public class passByValue {
+    public static void main(String[] args) {
+
+        // Pass by value
+
+        String name="Akash";
+        swap(name);
+        System.out.println(name);
+
+
+    }
+    static void swap(String naam){
+        naam="Abhi";
+
+    }
+
+}
