97a58a78baedb3e4974b328489bd676b6febafe9
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-08-26 11:22:25 +0530

    LCM of two numbers

---
+++ LCM.java
@@ -0,0 +1,34 @@
+package Conditional_Loops;
+import java.util.Scanner;
+public class LCM {
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int a=in.nextInt();
+        System.out.println("Enter second number : ");
+        int b=in.nextInt();
+
+        int factor1=0;
+        int factor2=0;
+        int hcf=0;
+        for(int i=1;i<=a;i++){
+            if(a%i==0){
+                factor1=i;
+            }
+        }
+        for(int i=1;i<=b;i++){
+            if(b%i==0){
+                factor2=i;
+                if(a%factor2==0){
+                    hcf=factor2;
+                }
+
+            }
+        }
+        System.out.print("Lcm of these two numbers is : "+(a*b)/hcf);
+
+
+
+    }
+}
