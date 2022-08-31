7c856f0a53ffd9f3235e08466a46513bcd7cb715
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-08-31 09:11:34 +0530

    Functions

---
+++ Functions/productOfTwoNumber.java
@@ -0,0 +1,26 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class productOfTwoNumber {
+    static int Product(int num1,int num2){
+        int product=num1*num2;
+        return product;
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int num1=in.nextInt();
+        System.out.println("Enter second number : ");
+        int num2=in.nextInt();
+
+
+        System.out.println("Product of above two number is : " +Product(num1,num2));
+
+    }
+}
+
+
