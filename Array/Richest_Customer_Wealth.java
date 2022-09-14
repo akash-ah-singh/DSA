897032e977c54cfc0e3bc64a555fd3bf8e3b2f21
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-09-14 21:24:10 +0530

    Array

---
+++ Array/Richest_Customer_Wealth.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Richest_Customer_Wealth {
+    public static void main(String[] args) {
+        int[][] arr= {{1,2,3},{3,2,1}};
+        System.out.println(maximumWealth(arr));
+
+    }
+    static int maximumWealth(int[][] accounts){
+        int maxwealth=0;
+
+        for(int row=0;row<accounts.length;row++){
+            int Sum=0;
+            for(int col=0;col<accounts[row].length;col++){
+                Sum=Sum+accounts[row][col];
+            }
+            if(Sum>maxwealth){
+                maxwealth=Sum;
+            }
+        }
+        return maxwealth;
+    }
+}
