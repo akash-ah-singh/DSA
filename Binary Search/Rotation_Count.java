6ec32204ef4c93d603a4edd8fad2fc21a0a3e1f7
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-10-09 10:37:38 +0530

    Binary Search

---
+++ Binary Search/Rotation_Count.java
@@ -0,0 +1,32 @@
+package Binary_Search;
+
+public class Rotation_Count {
+    public static void main(String[] args) {
+//        int [] arr={1,2,3,4,5,6,7,8,9};
+        int [] arr={4,5,6,7,0,1,2};
+        int pivot=findPivot(arr);
+        System.out.println(pivot+1);
+
+    }
+
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
