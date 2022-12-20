69198fe84532897658f179b2a32dc734098d4163
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-12-20 12:15:56 +0530

    Sorting

---
+++ Sorting/Find_All_Numbers_Disappeared_in_an_Array.java
@@ -0,0 +1,46 @@
+package Sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Find_All_Numbers_Disappeared_in_an_Array {
+    public static void main(String[] args) {
+        int[] arr={4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+
+    }
+
+
+
+    public static List<Integer> findDisappearedNumbers(int[] nums){
+
+        int i = 0;
+        while ( i <nums.length ) {
+            int correct =nums[i]-1;
+
+            if(nums[i]!=nums[correct]){
+                swap(nums, i , correct );
+            }
+            else{
+                i++;
+            }
+
+        }
+        List<Integer> ans= new ArrayList<>();  // array list is created because output can varry till any numbers of array
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j]!=j+1){
+                ans.add(j+1);
+            }
+
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}
